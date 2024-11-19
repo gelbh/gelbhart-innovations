ec30f69a2f348f3a0c98632c50360aa4003be4d6
gelbh <gelbharttomer@gmail.com>
2024-11-19 01:41:57 +0100

    changed description variable

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,6 @@
 class DocumentsController < ApplicationController
   def tos
     @title = "Terms & Conditions and Privacy Policy"
-    @description = "Use of this website is subject to all of the terms and conditions of this legal notice and to all applicable laws."
+    @desc = "Use of this website is subject to all of the terms and conditions of this legal notice and to all applicable laws."
   end
 end
