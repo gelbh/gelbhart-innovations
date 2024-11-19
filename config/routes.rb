79fe3c19af59b888127b6ab71e8b4d0fc09ed19e
gelbh <gelbharttomer@gmail.com>
2024-11-19 01:37:01 +0100

    Added TOS and Privacy Policy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'services', controller: 'pages'
   get 'team', controller: 'pages'
   get 'contacts', controller: 'pages'
+  
+  get 'tos', to: 'documents#tos'
 
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
