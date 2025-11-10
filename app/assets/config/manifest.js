8e0135e1369a02962999fb922f3007ca5e60b4e6
gelbh <gelbharttomer@gmail.com>
2025-11-10 20:57:38 +0000

    chore(assets): remove unused root vendor directory and manifest reference

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 
 //= link_directory ../vendor
 //= link_directory ../vendor/lightgallery
