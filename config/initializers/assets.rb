1fc6abb0b85a151d86432d8bd0550d96b18447c6
gelbh <gelbharttomer@gmail.com>
2025-12-01 19:34:07 +0000

    fix(assets): add .erb extension to webmanifest precompile path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "vendor")
 
 # Precompile ERB-processed webmanifest
-Rails.application.config.assets.precompile += %w(favicon/site.webmanifest)
+Rails.application.config.assets.precompile += %w(favicon/site.webmanifest.erb)
