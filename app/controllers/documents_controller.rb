28eb4041500a6e54cc87a7ac7086ef8408ad10cd
gelbh <gelbharttomer@gmail.com>
2025-12-02 15:47:48 +0000

    feat(documents): redesign tos page with card layout and jarallax

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,8 +1,11 @@
 class DocumentsController < ApplicationController
+  include JarallaxConfiguration
+
   def tos
     assign_page_metadata(
       title: "Terms & Conditions and Privacy Policy",
       description: "Use of this website is subject to all of the terms and conditions of this legal notice and to all applicable laws."
     )
+    assign_jarallax(image: AppConstants::JARALLAX_IMAGES[:services])
   end
 end
