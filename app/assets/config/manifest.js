610d2d4283860e64b8eab1542cf2bd83e07efb21
gelbh <gelbharttomer@gmail.com>
2025-12-30 22:27:51 +0100

    fix(assets): add node_modules path for flag-icons symlink

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -19,7 +19,6 @@
 //= link_directory ../vendor/jarallax/dist
 //= link_directory ../vendor/parallax-js/dist
 //= link_directory ../vendor/@lottiefiles/lottie-player/dist
-//= link_tree ../vendor/flag-icons/css .css
 
 //= link_tree ../favicon
 //= link_tree ../json
